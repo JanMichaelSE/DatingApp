bc716f5b82119aa2a300453443a95731da0b8912
JanMichaelSe <janm.montalvo@gmail.com>
2020-03-28 14:47:15 -0400

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore; // (DevNote): Downloaded with NuggetPacketManager
+
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+
+        //This is the table name
+        public DbSet<Value> Values { get; set; }
+
+    }
+}
