628316b3b75f53fcf7c06738e57e25f099e76857
JanMichaelSe <janm.montalvo@gmail.com>
2020-03-29 17:31:16 -0400

    Seeded Data , Created User Interface, Controller

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -10,6 +10,7 @@ namespace DatingApp.API.Data
         //This is the table name
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
 
     }
 }
---
+++ DatingApp.API/Models/Photo.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace DatingApp.API.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+        public bool IsMain { get; set; }
+
+        // This is how make sure if a user is deleted then the photos will also be deleted
+        public User User { get; set; }
+        public int UserId { get; set; }
+    }
+}
