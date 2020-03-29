a2793c57f2387b0db13ca6659a1038db32135cf3
JanMichaelSe <janm.montalvo@gmail.com>
2020-03-28 23:37:26 -0400

    Added Register and Login forms

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  // Property
+  username: any;
+  password: any;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {}
+
+  login() {
+    let model = {
+      username: this.username,
+      password: this.password
+    };
+    this.authService.login(model).subscribe(
+      next => {
+        console.log('Logged in');
+      },
+      error => {
+        console.log('Failed To login');
+      }
+    );
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('Logged out!');
+  }
+}
